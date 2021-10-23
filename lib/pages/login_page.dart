8a8fb55c2ff4a1db8d631eaf5441a471d76bb2ea
TejasParse <tejasparse1@gmail.com>
2021-10-23 19:20:14 +0530

    Day 5 getting rid of blue color

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -7,15 +8,45 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-            "Login Page",
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: Colors.red,
-          ),
-          textScaleFactor: 3.0,
-        ),
+        child: Column(
+          children: [
+            Image.asset("assets/images/login.png",height: 200,),
+            Text(
+              "Welcome",
+              style: GoogleFonts.oswald(fontSize: 20),
+              textScaleFactor: 2,
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30,horizontal: 30),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter User Name",
+                      labelText: "User Name"
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password"
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(
+                onPressed: (){
+                  Navigator.pushNamed(context,"/Home");
+                },
+                child: Text("Login")
+            )
+          ],
+        )
       ),
     );
   }
