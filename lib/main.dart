87f815c101a0eaccb1779682ca468bf052ce56f8
TejasParse <tejasparse1@gmail.com>
2021-10-23 22:46:50 +0530

    Day 5 Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:catalog/pages/home_page.dart';
 import 'package:catalog/pages/login_page.dart';
+import 'package:catalog/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,8 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-          "/" : (context) => LoginPage(),
-        "/Home" : (context) => HomePage(),
+          MyRoutes.loginRoute : (context) => LoginPage(),
+        MyRoutes.homeRoute : (context) => HomePage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:catalog/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -7,46 +8,51 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Image.asset("assets/images/login.png",height: 200,),
-            Text(
-              "Welcome",
-              style: GoogleFonts.oswald(fontSize: 20),
-              textScaleFactor: 2,
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30,horizontal: 30),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter User Name",
-                      labelText: "User Name"
-                    ),
-                  ),
-                  SizedBox(height: 20,),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter Password",
-                      labelText: "Password"
-                    ),
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset("assets/images/login.png",height: 200,),
+              Text(
+                "Welcome",
+                style: GoogleFonts.oswald(fontSize: 20,color: Colors.blue[400]),
+                textScaleFactor: 2,
               ),
-            ),
-            SizedBox(height: 20,),
-            ElevatedButton(
-                onPressed: (){
-                  Navigator.pushNamed(context,"/Home");
-                },
-                child: Text("Login")
-            )
-          ],
-        )
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30,horizontal: 30),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter User Name",
+                        labelText: "User Name"
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password"
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(50,50),
+                ),
+                  onPressed: (){
+                    Navigator.pushNamed(context,MyRoutes.homeRoute);
+                  },
+                  child: Text("Login")
+              )
+            ],
+          )
+        ),
       ),
     );
   }
