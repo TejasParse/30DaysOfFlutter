c3b726f86b6647234da5d08fa71100038b9b145f
TejasParse <tejasparse1@gmail.com>
2021-11-02 00:08:07 +0530

    Day11 Done uwu

---
+++ lib/widgets/list_item_widget.dart
@@ -0,0 +1,11 @@
+import 'package:catalog/utils/items.dart';
+import 'package:flutter/material.dart';
+
+class listItemwidget extends StatelessWidget {
+  const listItemwidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
